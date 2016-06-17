c59d72772fc9d2450cb2a9a47e28626f9bc5ae32
Andrew Sands <andrew@theatrix.org.nz>
2016-06-17 12:19:59 +1200

    Add workfile deletion question code. Tidy up.

--- myVoucher.sh
+++ myVoucher.sh
@@ -66,4 +66,14 @@ cat $OUTFILENAME
 
 ## Tidy Up at the end. Remove workfile and return to home directory.
 #rm $WORKFILENAME
-cd
+### Refer: http://stackoverflow.com/questions/1885525	\
+###				/how-do-i-prompt-a-user-for-confirmation-in-bash-script
+echo "Delete $WORKFILENAME"
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # do dangerous stuff
+    #rm $WORKFILENAME
+fi
+cd # return to home directory.
