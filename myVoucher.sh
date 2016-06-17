5076860f5deba92a82e3e9f317348d286291a945
Andrew Sands <andrew@theatrix.org.nz>
2016-06-17 12:30:16 +1200

    Corrected syntax error. My bad typing. :-)

--- myVoucher.sh
+++ myVoucher.sh
@@ -68,12 +68,15 @@ cat $OUTFILENAME
 #rm $WORKFILENAME
 ### Refer: http://stackoverflow.com/questions/1885525	\
 ###				/how-do-i-prompt-a-user-for-confirmation-in-bash-script
-echo "Delete $WORKFILENAME"
+echo "Delete $WORKFILE"
 read -p "Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     # do dangerous stuff
-    #rm $WORKFILENAME
+    rm $WORKFILE;
+    echo "Ok. $WORKFILE Deleted.";
+else
+	echo "Keeping $WORKFILE";
 fi
 cd # return to home directory.
