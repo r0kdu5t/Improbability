87158cc0013d8b30a833220ecc4120511d74a3be
Andrew Sands <andrew@theatrix.org.nz>
2016-09-15 20:51:40 +1200

    Completed first working version of game. Debugged and corrected typo.

--- wumpus.py
+++ wumpus.py
@@ -7,7 +7,7 @@ while player_location == wumpus_location:
 	player_location = choice(cave_numbers)
 
 print("Welcome to Hunt the Wumpus!")
-print("You can see", len(caves), "caves")
+print("You can see", len(cave_numbers), "caves")
 print("To play, just type the number")
 print("of the cave you wish to enter next.")
 
@@ -21,8 +21,7 @@ while True:
 	if (not player_input.isdigit() or int(player_input) not in cave_numbers):
 		print(player_input, " is not a cave!")
 	else:
-		player_location == int(player_input)
+		player_location = int(player_input)
 		if player_location == wumpus_location:
 			print("Aargh! You got eaten by a wumpus!")
 			break
-			
