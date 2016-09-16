a47fb251cb6deb9f01210ecd4aae5d283ff0bf7b
Andrew Sands <andrew@theatrix.org.nz>
2016-09-16 17:40:58 +1200

    Refactored the wumpus game. Now to test!

--- wumpus.py
+++ wumpus.py
@@ -1,4 +1,4 @@
-import random
+from random import choice
 
 def create_tunnel(cave_from, cave_to):
 	""" Create a tunnel between cave_from and cave_to.  """
@@ -68,34 +68,16 @@ def get_next_location():
 	else:
 		return int(player_input)
 
-
-
+cave_numbers = range(0, 20)
 unvisited_caves = range(0, 20)
-visited_caves = [0]
-unvisited_caves.remove(0)
+visited_caves = []
+caves = setup_caves(cave_numbers)
 
-while unvisited_caves != []:
-	i = choice(visited_caves)
-	if len(caves[i] >= 3):
-		continue
-
-	next_cave = choice(unvisited_caves)
-	caves[i].append(next_cave)
-	caves[next_cave].append(i)
-
-	visited_caves.append(next_cave)
-	unvisited_caves.remove(next_cave)
-
-
-
-for i in cave_numbers:
-	while len(caves[i]) < 3:
-		passage_to = choice(cave_numbers)
-		caves[i].append(passage_to)
-
-	for number in cave_numbers:
-		print(number, ":", caves[number])
-	print("----------")		
+visit_cave(0)
+print_caves()
+link_caves()
+print_caves()
+finish_caves()
 
 wumpus_location = choice(cave_numbers)
 player_location = choice(cave_numbers)
@@ -108,8 +90,10 @@ print("To play, just type the number")
 print("of the cave you wish to enter next.")
 
 while True:
-
-
-	print("Which cave next?")
-	
-
+	print_location(player_location)
+	new_location = get_next_location()
+	if new_location isn't None:
+		player_location = new_location
+	if player_location == wumpus_location:
+		print("Aargh! You got eaten by a wumpus!")
+		break
