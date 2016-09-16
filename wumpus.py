0191e255079abafc25261eff34bcdbcec5f68925
Andrew Sands <andrew@theatrix.org.nz>
2016-09-16 16:49:55 +1200

    Linked caves network or something.

--- wumpus.py
+++ wumpus.py
@@ -1,15 +1,39 @@
-from random import choice
+import random
 
 cave_numbers = range(0, 20)
 caves = []
 for i in cave_numbers:
 	caves.append([])
 
+unvisited_caves = range(0, 20)
+visited_caves = [0]
+unvisited_caves.remove(0)
+
+while unvisited_caves != []:
+	i = choice(visited_caves)
+	if len(caves[i] >= 3):
+		continue
+
+	next_cave = choice(unvisited_caves)
+	caves[i].append(next_cave)
+	caves[next_cave].append(i)
+
+	visited_caves.append(next_cave)
+	unvisited_caves.remove(next_cave)
+
+	for number in cave_numbers:
+		print(number, ":", caves[number])
+	print("----------")
+
 for i in cave_numbers:
-	for j in range(3):
+	while len(caves[i]) < 3:
 		passage_to = choice(cave_numbers)
 		caves[i].append(passage_to)
-print caves
+
+	for number in cave_numbers:
+		print(number, ":", caves[number])
+	print("----------")		
+
 wumpus_location = choice(cave_numbers)
 player_location = choice(cave_numbers)
 while player_location == wumpus_location:
@@ -31,7 +55,7 @@ while True:
 	if (not player_input.isdigit() or int(player_input) not in caves[player_location]):
 		print(player_input, " ?")
 		print("That's not a direction that I can see!")
-		
+
 	else:
 		player_location = int(player_input)
 		if player_location == wumpus_location:
